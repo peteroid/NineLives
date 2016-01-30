08777f4ac7f8cadc7cae2ec02c83348eb90fb282
KTZimmerle <kzimmerl@uci.edu>
2016-01-30 00:05:43 -0800

    TileSystem GenerateTileMap function complete
    Function will still need to be fine tuned, but it will work for testing
    purposes

--- Assets/Scripts/TileSystem.cs
+++ Assets/Scripts/TileSystem.cs
@@ -7,23 +7,40 @@ public class TileSystem : MonoBehaviour {
      * Tiles are as follows:
      * 0 - passable
      * 1 - impassable
+     * - KTZ
      */
 
+    public GameObject PassableTile;
+    public GameObject WallTile;
     public bool passable;
-    public int[][] tileMapTest = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                                   1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                                   1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                                   1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                                   1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                                   1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                                   1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                                   1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                                   1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                                   1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+    public int[][] tileMapTest = new int[][]{ new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                                              new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                                              new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                                              new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                                              new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                                              new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                                              new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                                              new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                                              new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                                              new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1} };
 
     public void GenerateTileMap()
     { 
         // Instantiate the tile types onto the game world, or placing them - KTZ
+        for(int i = 0; i < 10; i++)
+            for(int j = 0; j < 10; j++)
+            {
+                Vector2 tilePos = new Vector2(0.0f + i, 0.0f + j);
+                Quaternion tileRot = Quaternion.identity;
+                if (tileMapTest[i][j] == 1)
+                {
+                    Instantiate(WallTile, tilePos, tileRot);
+                }
+                else
+                {
+                    Instantiate(PassableTile, tilePos, tileRot);
+                }
+            }
     }
 
     public void GenerateCollision()
@@ -34,7 +51,7 @@ public class TileSystem : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        GenerateTileMap();
 	}
 	
 	// Update is called once per frame
