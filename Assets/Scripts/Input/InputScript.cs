5ef99fb3e14eff7e8e036ff5bdf5228d7694ddf7
PETEroid <selllfocard@gmail.com>
2016-01-30 00:47:30 -0800

    Added simple input

---
+++ Assets/Scripts/Input/InputScript.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class InputScript : MonoBehaviour {
+
+	private InputInterface input;
+	private string errorInterfaceNotImplemented = "Not implemented interface";
+	private string errorObjectNotFound = "Object not found";
+
+	// the user clicked up
+	public void Up() {
+		Debug.Log ("Up");
+		if (input == null) {
+			Debug.Log (errorObjectNotFound);
+		} else if (! (input is InputInterface)) {
+			Debug.Log (errorInterfaceNotImplemented);
+		} else {
+			((InputInterface) input).Up ();
+		}
+	}
+
+	// the user clicked left
+	public void Left() {
+		Debug.Log ("Left");
+		if (input == null) {
+			Debug.Log (errorObjectNotFound);
+		} else if (! (input is InputInterface)) {
+			Debug.Log (errorInterfaceNotImplemented);
+		} else {
+			((InputInterface) input).Left ();
+		}
+	}
+
+	// the user clicked down
+	public void Down() {
+		Debug.Log ("Down");
+		if (input == null) {
+			Debug.Log (errorObjectNotFound);
+		} else if (! (input is InputInterface)) {
+			Debug.Log (errorInterfaceNotImplemented);
+		} else {
+			((InputInterface) input).Down ();
+		}
+	}
+
+	// the user clicked right
+	public void Right() {
+		Debug.Log ("Right");
+		if (input == null) {
+			Debug.Log (errorObjectNotFound);
+		} else if (! (input is InputInterface)) {
+			Debug.Log (errorInterfaceNotImplemented);
+		} else {
+			((InputInterface) input).Right ();
+		}
+	}
+
+	public void SetInputInterface (InputInterface input) {
+		this.input = input;
+	}
+}
---
+++ Assets/Scripts/Input/InputTemplate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// add this script to the object which can be controlled by the input
+public class InputTest : MonoBehaviour, InputInterface {
+
+	// drag and drop the input to this GameObject
+	public GameObject input;
+
+	public void Up () {
+	}
+
+	public void Left () {
+	}
+
+	public void Right () {
+	}
+
+	public void Down () {
+	}
+
+	void Start () {
+		input.GetComponent<InputScript> ().SetInputInterface (this);
+	}
+}
