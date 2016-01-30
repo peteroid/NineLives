3c7f5e3ba3912b9a52cb7d1373580c9562faa7fa
KTZimmerle <kzimmerl@uci.edu>
2016-01-29 23:33:18 -0800

    TileSystem WIP
    Began work on the TileSystem. It is work in progress.

---
+++ Assets/Scripts/TileSystem.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileSystem : MonoBehaviour {
+
+    /*
+     * Tiles are as follows:
+     * 0 - passable
+     * 1 - impassable
+     */
+
+    public bool passable;
+    public int[][] tileMapTest = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                   1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                                   1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                                   1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                                   1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                                   1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                                   1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                                   1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                                   1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                                   1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+
+    public void GenerateTileMap()
+    { 
+        // Instantiate the tile types onto the game world, or placing them - KTZ
+    }
+
+    public void GenerateCollision()
+    { 
+        // Put collision on any tiles that are impassible - KTZ
+    }
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
