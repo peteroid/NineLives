cb5ec5f87075f58714c82cb0a16ecd9bbdebd1a8
KTZimmerle <kzimmerl@uci.edu>
2016-01-30 10:23:24 -0800

    TileSystem, IsometricCamera Updated.

--- Assets/Scripts/IsometricCamera.cs
+++ Assets/Scripts/IsometricCamera.cs
@@ -5,7 +5,8 @@ public class IsometricCamera : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        transform.Rotate(Vector3.forward * -45.0f);
+        transform.position = new Vector3(3, 3, -10);
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/TileSystem.cs
+++ Assets/Scripts/TileSystem.cs
@@ -6,14 +6,16 @@ public class TileSystem : MonoBehaviour {
     /*
      * Tiles are as follows:
      * 0 - passable
-     * 1 - impassable
+     * 1 - wall (impassable)
+     * 2 - door (impassable -> passable)
      * - KTZ
      */
 
     public GameObject PassableTile;
     public GameObject WallTile;
+    public GameObject DoorTile;
     public bool passable;
-    public int[][] tileMapTest = new int[][]{ new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    public int[][] tileMapTest = new int[][]{ new int[]{1, 1, 1, 1, 2, 1, 1, 1, 1, 1},
                                               new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
                                               new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
                                               new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
@@ -30,17 +32,27 @@ public class TileSystem : MonoBehaviour {
         for(int i = 0; i < 10; i++)
             for(int j = 0; j < 10; j++)
             {
-                Vector2 tilePos = new Vector2(0.0f + i, 0.0f + j);
+                Vector3 tilePos = new Vector3(0.0f + i, 0.0f + j, 0.0f);
                 Quaternion tileRot = Quaternion.identity;
                 if (tileMapTest[i][j] == 1)
                 {
-                    Instantiate(WallTile, tilePos, tileRot);
+                    GameObject NewTile = Instantiate(WallTile, tilePos, tileRot) as GameObject;
+                    NewTile.transform.parent = gameObject.transform;
+                }
+                else if(tileMapTest[i][j] == 2)
+                {
+                    GameObject NewTile = Instantiate(DoorTile, tilePos + new Vector3(0.0f, 0.0f, 0.25f), tileRot) 
+                        as GameObject;
+                    NewTile.transform.parent = gameObject.transform;
                 }
                 else
                 {
-                    Instantiate(PassableTile, tilePos, tileRot);
+                    GameObject NewTile = Instantiate(PassableTile, tilePos + new Vector3(0.0f, 0.0f, 0.25f), tileRot) 
+                        as GameObject;
+                    NewTile.transform.parent = gameObject.transform;
                 }
             }
+        transform.Rotate(35.0f, 315.0f, 345.0f);
     }
 
     public void GenerateCollision()
