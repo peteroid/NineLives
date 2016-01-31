3bbe75b3793c57503242db4c6f14b37deeaa5080
Reina <reinaraneko@gmail.com>
2016-01-31 14:47:30 -0800

    Interface for tile movement objects

---
+++ Assets/Scripts/ITilePlaceable.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface ITilePlaceable
+{
+    int GetX();
+    int GetY();
+
+    bool AllowIncomingMove(ITilePlaceable incomingPlaceable, int dirX, int dirY);
+    void TryIncomingMove(ITilePlaceable incomingPlaceable, int dirX, int dirY);
+}
