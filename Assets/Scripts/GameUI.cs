c3ec8194ff9210ddea3b72a69c093e9092ee9946
KTZimmerle <kzimmerl@uci.edu>
2016-01-30 17:12:59 -0800

    GameUI Updated

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -4,14 +4,17 @@ using UnityEngine.UI;
 
 public class GameUI : MonoBehaviour {
 
-    Text m_Level;
-    Text m_Message;
-    Text m_Story;
-    Text m_DoorStatus;
+    public Text m_Level;
+    public Text m_Message;
+    public Text m_Story;
+    public Text m_DoorStatus;
 
 	// Use this for initialization
 	void Start () {
-	    
+        UpdateLevelText(1);
+        UpdateMessage("Solve the Puzzle!");
+        StoryText();
+        printDoorStatus(false);
 	}
 	
 	// Update is called once per frame
@@ -35,5 +38,16 @@ public class GameUI : MonoBehaviour {
     {
         UnityEngine.UI.Text text = m_Story.GetComponent<Text>();
         m_Story.text = "You are a cat, seeking to become human! Through the power of magic, will you transform into a human. Solve strange puzzles throughout your journey on becoming a human. Collect golden humans to become human!";
+    }
+
+    public void printDoorStatus(bool isOpen)
+    { 
+        UnityEngine.UI.Text text = m_DoorStatus.GetComponent<Text>();
+        if (isOpen)
+            m_DoorStatus.text = "The door is open!";
+        else
+            m_DoorStatus.text = "The door is closed.";
+
+
     }
 }
