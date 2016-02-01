acecfc2f067f42bbe1ff3648f4e61d082850a77f
PETEroid <selllfocard@gmail.com>
2016-01-31 16:46:44 -0800

    Fix dialogue

--- Assets/Scripts/DialogueScript.cs
+++ Assets/Scripts/DialogueScript.cs
@@ -11,8 +11,11 @@ public class DialogueScript : MonoBehaviour {
 	public Tile.TerrainType lastType;
 	public Text dialogue1;
 	public Text dialogue2;
-	public Image spriteCat;
-	public Image spriteOther;
+	public Image image1;
+	public Image image2;
+	public Sprite spriteCat;
+	public Sprite spriteOther;
+	public SharedDataScript sharedDataObject;
 
 	public void LoadGameScene () {
 		SceneManager.LoadScene("UnifiedGame");
@@ -22,21 +25,32 @@ public class DialogueScript : MonoBehaviour {
 	void Start () {
 		Invoke ("LoadGameScene", delay);
 
-		TextAsset levelFile = Resources.Load<TextAsset>("Dialogue/StoryData");
-//		JSONNode jsonObj = JSON.Parse(levelFile.text);
-//
-//		JSONArray data = jsonObj["cat"].AsArray;
-//		if (lastType == Tile.TerrainType.kHumanExit)
-//			data = jsonObj["human"].AsArray;
-//
-//		JSONArray order = data["order"].AsArray;
-//		 
-//		for (int i = 0; i < order.Count; i++)
-//		{
-//			Debug.Log (order[i]);
-//		}
+		TextAsset levelFile = Resources.Load<TextAsset>("Levels/StoryData");
+		JSONNode jsonObj = JSON.Parse(levelFile.text);
 
+		JSONArray data = jsonObj["catStory"].AsArray;
+		if (lastType == Tile.TerrainType.kHumanExit)
+			data = jsonObj["humanStory"].AsArray;
 
+		JSONArray order = data[sharedDataObject.levelIndex]["order"].AsArray;
+		if (order.Count > 0) {
+			dialogue1.text = data[sharedDataObject.levelIndex][order[0]];
+//			Debug.Log (order [0].Value == "cat");
+			if (order [0].Value == "cat")
+				image1.overrideSprite = spriteCat;
+			else if (order [0].Value == "other")
+				image1.overrideSprite = spriteOther;
+		}
+
+		if (order.Count > 1) {
+			dialogue2.text = data[sharedDataObject.levelIndex][order[1]];
+			Debug.Log (order [1].Value);
+			if (order [1].Value == "cat")
+				image2.overrideSprite = spriteCat;
+			else if (order [1].Value == "other")
+				image2.overrideSprite = spriteOther;
+			image2.gameObject.SetActive (true);
+		}
 	}
 	
 	// Update is called once per frame
