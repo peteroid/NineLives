7310793ce59ef9a2dd6ccbaba55c76bbaf1afdad
PETEroid <selllfocard@gmail.com>
2016-01-31 17:21:55 -0800

    add ending

---
+++ Assets/CatEnding2Script.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatEnding2Script : MonoBehaviour {
+
+	void LoadScene(){
+//		SceneManager.LoadScene ("");
+	}
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("LoadScene", 7f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/CatEndingScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class CatEndingScript : MonoBehaviour {
+
+	void LoadScene(){
+		SceneManager.LoadScene ("CatEndingScene2");
+	}
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("LoadScene", 7f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
