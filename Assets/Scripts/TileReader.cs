2325da64ea11a76349eca85a238c0c9b7b1c5ae8
bazaam <krazykid4172>
2016-02-03 18:22:40 -0800

    Cleanup, reorganization, and start of level builder script

---
+++ Assets/Scripts/TileReader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileReader : MonoBehaviour {
+    public GameObject[] tileMapSave;
+    public GameObject[] toyMapSave;
+    // Use this for initialization
+    public void GetObjecID(GameObject obj);
+        {
+            
+        }
+	void Start ()
+    {
+       tileMapSave = GameObject.FindGameObjectsWithTag("Tile");
+       toyMapSave = GameObject.FindGameObjectsWithTag("Placeable");
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
