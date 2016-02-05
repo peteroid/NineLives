cc91a33a74b46d9589531aaa2ea5cea6cf175e49
bazaam <bazaam1@gmail.com>
2016-02-05 00:46:01 -0800

    First pass at new tile format

---
+++ Assets/TileConverter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileConverter : MonoBehaviour {
+
+    public GameObject[] tileMap;
+    public GameObject[] toyMap;
+
+
+    public TileConverter store(GameObject tile, params GameObject[] toys)
+    {
+
+    }
+
+    public TileConverter generate()
+    {
+
+    }
+
+}
